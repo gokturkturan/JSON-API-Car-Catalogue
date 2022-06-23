f4c0f6f6dd1de878d91425ad52d7eec62ea19cdc
gokturkturan <gktrktrn673@gmail.com>
2022-06-24 01:34:40 +0300

    htacces and functions
    functions includes SetHeader and HttpStatus

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+    
+    function HttpStatus($code) {
+	    $status = array( 200 => 'OK', 400 => 'Fail', 500 => 'Internal Server Error');
+        return $status[$code] ? $status[$code] : $status[500];
+    }
+
+    function SetHeader($code){
+        header("HTTP/1.1 ".$code." ".HttpStatus($code));
+        header("Content-Type: application/json; charset=utf-8");
+    }
+
+?>
